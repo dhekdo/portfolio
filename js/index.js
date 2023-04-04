ab85cf78fff076f19f69125efbfabc62d390c3e5
dhekdo <daae0924@naver.com>
2023-04-04 17:40:11 +0900

    2023.04.04

--- js/index.js
+++ js/index.js
@@ -1,3 +1,15 @@
+// 섹션이동
+var swiper = new Swiper(".mySwiper", {
+  direction: "vertical",
+  slidesPerView: 1,
+  spaceBetween: 30,
+  mousewheel: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
 // 인트로
 let text = document.querySelector('#text');
 
@@ -5,9 +17,9 @@ text.setAttribute('data-text',text.innerHTML);
 
 // document.body.innerHTML += '<div style="position: absolute; bottom: 0; left: 0; right: 0; padding: .5em; font-size: 12px; font-family: monospace; text-align: center; pointer-event: none;">Click anywhere to re-run</div>';
 
-var lr = document.querySelector('html');
+var lr = document.querySelector('#text');
 window.addEventListener('click',()=>{
-  var newone = lr.cloneNode(true);
+  var newone = text.cloneNode(true); 
   lr.parentNode.replaceChild(newone, lr);
   lr = newone;
 });
