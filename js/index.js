b35ac9ded9dfb96e543779c75c547574de780acc
dhekdo <daae0924@naver.com>
2023-04-06 16:41:50 +0900

    2023.04.06

--- js/index.js
+++ js/index.js
@@ -53,12 +53,12 @@ items.forEach((item, i) => {
 
 // 아이템 클릭시 버튼 구현
 $(".item").click(function(){
-  $(this).children().fadeIn(800).toggleClass("on").end().siblings().children().hide(800);
+  $(this).children("button").fadeIn(800).end().siblings().children("button").hide(800);
  var size1 = Math.floor($(this).width());
  var size2 = Math.floor(window.innerWidth*0.25);
 
 if( size1 > size2 ){
-  $(this).children().hide();
+  $(this).children("button").hide();
 }
 });
 
