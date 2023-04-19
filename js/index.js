6b0e548b93f7611d968bb3b6fca6fc376a34ba91
dhekdo <daae0924@naver.com>
2023-04-19 18:19:04 +0900

    2023.04.19_3

--- js/index.js
+++ js/index.js
@@ -1,17 +1,31 @@
 window.onload = function(){
-    // 프로필 클릭시 노출
+    // 프로필 버튼 클릭시 노출
     $(".profile_link").click(function(){
         $(".profile").fadeIn(1000);
         $(".logo .logo_text").css({animation: "slideUp 0.8s ease-in-out forwards", display: "inline-block"});
     });
     // 닫기 버튼 클릭
-    $(".exit").click(function(){
+    $(".exit_btn").click(function(){
         $(".profile").fadeOut();
         $(".logo .logo_text").css({animation: "slideDown 0.8s ease-in-out forwards", display : ""});
     }).mouseover(function(){
-        $(".exit span").css({"-webkit-text-stroke" : "1px #fff", color:"#4801ff"});
+        $(".exit_btn span").css({"-webkit-text-stroke" : "1px #fff", color:"#4801ff"});
     }).mouseout(function(){
-        $(".exit span").css({color : ""});
+        $(".exit_btn span").css({color : ""});
+    });
+
+    // 컨텐츠 링크 클릭시 노출
+    $(".title>a").click(function(){
+        $(".logo .logo_text").css({animation: "slideUp 0.8s ease-in-out forwards", color:"#000"});
+        $("nav a, nav li").css({color : "#000", transition: "all 1s"})
+        $(".content").fadeIn();
+    });
+
+    // 뒤로가기 버튼 클릭
+    $(".pre_btn").click(function(){
+        $(".content").fadeOut();
+        $(".logo .logo_text").css({animation: "slideDown 0.8s ease-in-out forwards", color: ""});
+        $("nav a, nav li").css({color : "", transition: "all 1s"})
     });
 
 }
