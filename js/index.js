cf6ef93ef45b3e5fa4948f6aa2a7509909f5ec7c
dhekdo <daae0924@naver.com>
2023-04-19 15:33:21 +0900

    2023.04.19_2

--- js/index.js
+++ js/index.js
@@ -1,14 +1,13 @@
 window.onload = function(){
     // 프로필 클릭시 노출
     $(".profile_link").click(function(){
-        $(".profile").fadeIn(function(){
-            $(".logo .logo_text").css({position:"fixed", top:"5%", left: "3%" , display:"inline-block"})
-        });
+        $(".profile").fadeIn(1000);
+        $(".logo .logo_text").css({animation: "slideUp 0.8s ease-in-out forwards", display: "inline-block"});
     });
-
+    // 닫기 버튼 클릭
     $(".exit").click(function(){
         $(".profile").fadeOut();
-        $(".logo .logo_text").css({position:"", top:"", left: "" , display:""});
+        $(".logo .logo_text").css({animation: "slideDown 0.8s ease-in-out forwards", display : ""});
     }).mouseover(function(){
         $(".exit span").css({"-webkit-text-stroke" : "1px #fff", color:"#4801ff"});
     }).mouseout(function(){
