68ae2a79bb2af40eb1e59c7b442f2cdeeda1c627
dhekdo <daae0924@naver.com>
2023-04-20 17:19:44 +0900

    2023.04.20_2

--- js/index.js
+++ js/index.js
@@ -17,16 +17,62 @@ window.onload = function(){
     // 컨텐츠 링크 클릭시 노출
     $(".main_link>li").click(function(){
         var titleIndex = $(this).index();
-        $(".logo .logo_text").css({animation: "slideUp 0.8s ease-in-out forwards", color:"#000"});
-        $("nav a, nav li").css({color : "#000", transition: "all 1s"})
+        $(".logo .logo_text").css({animation: "slideUp 0.8s ease-in-out forwards"});
+        $(".info li:last-child").fadeOut();
         $(".content").fadeIn(function(){
             $(".slide_wrap>div").eq(titleIndex).fadeIn();
+            // 링크마다 노출 다르게
+            if( $(".slide_mini1").css("display") == "block" ){
+                $(".content_wrap").css("background" , "#ea6852");
+                $(".logo .logo_text").css({color:"#000"});
+                $("nav a, nav li").css({color : "#000", transition: "all 0.5s"});
+                $(".slide_box span").css({"-webkit-text-stroke":"2px #000"});
+                $(".pre_btn i").css({color:"#000"});
+            }
+            else if( $(".slide_mini2").css("display") == "block" ){
+                $(".content_wrap").css("background" , "#00437b");
+                $(".logo .logo_text").css({color:"#fff"});
+                $("nav a, nav li").css({color : "#fff", transition: "all 0.5s"});
+                $(".slide_box span").css({"-webkit-text-stroke":"2px #fff"});
+                $(".pre_btn i").css({color:"#fff"});
+            }
+            else if( $(".slide_mini3").css("display") == "block" ){
+                $(".content_wrap").css("background" , "#ffeb60");
+                $(".logo .logo_text").css({color:"#000"});
+                $("nav a, nav li").css({color : "#000", transition: "all 0.5s"});
+                $(".slide_box span").css({"-webkit-text-stroke":"2px #fff"});
+                $(".pre_btn i").css({color:"#000"});
+            }
+            else if( $(".slide_mini4").css("display") == "block" ){
+                $(".content_wrap").css("background" , "#F7F5EC");
+                $(".logo .logo_text").css({color:"#000"});
+                $("nav a, nav li").css({color : "#000", transition: "all 0.5s"});
+                $(".slide_box span").css({"-webkit-text-stroke":"2px #000"});
+                $(".pre_btn i").css({color:"#000"});
+            }
+            else if( $(".slide_mini5").css("display") == "block" ){
+                $(".content_wrap").css("background" , "#F9E6D3");
+                $(".logo .logo_text").css({color:"#000"});
+                $("nav a, nav li").css({color : "#000", transition: "all 0.5s"});
+                $(".slide_box span").css({"-webkit-text-stroke":"2px #fff"});
+                $(".pre_btn i").css({color:"#000"});
+            }
+            else if( $(".slide_mini6").css("display") == "block" ){
+                $(".content_wrap").css("background" , "#333");
+                $(".logo .logo_text").css({color:"#fff"});
+                $("nav a, nav li").css({color : "#fff", transition: "all 0.5s"});
+                $(".slide_box span").css({"-webkit-text-stroke":"2px #fff"});
+                $(".pre_btn i").css({color:"#fff"});
+            }
         });
     });
+
+   
 
     // 뒤로가기 버튼 클릭
     $(".pre_btn").click(function(){
         $(".slide_wrap>div").fadeOut();
+        $(".info li:last-child").fadeIn();
         $(".content").fadeOut();
         $(".logo .logo_text").css({animation: "slideDown 0.8s ease-in-out forwards", color: ""});
         $("nav a, nav li").css({color : "", transition: "all 1s"})
